34a46bfa274c4cf4fa2908b5e5a0471d2d2ba3ac
Dharmaraj M <mdharmaraj1313@gmail.com>
2021-11-05 18:20:19 +0530

    Swagger added.

--- src/main/java/com/springboot/springbooth2/SpringbootH2Application.java
+++ src/main/java/com/springboot/springbooth2/SpringbootH2Application.java
@@ -1,9 +1,12 @@
 package com.springboot.springbooth2;
 
+import com.springboot.springbooth2.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class SpringbootH2Application {
 
     public static void main(String[] args) {
